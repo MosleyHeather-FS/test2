e20fa318fec3675a93b783e5807b2edf6d994337
Heather Mosley <hmosley@student.fullsail.edu>
2022-04-10 17:52:20 -0500

    Added a post, patch by id, get by id, and delete by id.

--- server.js
+++ server.js
@@ -2,10 +2,56 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get('/:id', (req, res, next) => {
+    const id = req.params.id;
     res.json({
-        message: "Did you GET it?"
+        message: "Using Get /",
+        metadata: {
+            host: req.hostname,
+            port: process.env.port,
+            method: req.method,
+            id: id,
+        }
     });
+})
+
+// post, patch, delete
+
+app.post('/:id', (req, res, next) => {
+    res.json({
+        message: "Using - Post",
+        metadata: {
+            host: req.hostname,
+            port: process.env.port,
+            method: req.method,
+        },
+    })
+})
+
+app.patch('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.json({
+        message: "Using - Patch",
+        metadata: {
+            host: req.hostname,
+            port: process.env.port,
+            method: req.method,
+            id: id,
+        }
+    })
+})
+
+app.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.json({
+        message: "Using - Patch",
+        metadata: {
+            host: req.hostname,
+            port: process.env.port,
+            method: req.method,
+            id: id,
+        }
+    })
 })
 
 
