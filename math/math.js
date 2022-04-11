9c68c5b943dc089091fadc6f50e5aacee80d2172
Heather Mosley <hmosley@student.fullsail.edu>
2022-04-10 20:21:33 -0500

    Added a new jest test for math assignment

---
+++ math/math.js
@@ -0,0 +1,15 @@
+const add = (n, i) => n + i;
+const subtract = (n, i) => n - i;
+const divide = (n, i) => n / i;
+const multiply = (n, i) => n * i;
+const squareRoot = (n) => Math.sqrt(n);
+const max = (n) => Math.max(n);
+
+module.exports = {
+    add,
+    subtract,
+    divide,
+    multiply,
+    squareRoot,
+    max
+}
---
+++ math/math.test.js
@@ -0,0 +1,24 @@
+const {add, subtract, multiply, divide, squareRoot, max} = require("./math");
+
+describe("Testing the math module", () => {
+
+    test("should add integers", () => {
+        expect(add(2, 4)).toBe(6);
+    });
+
+    test("should subtract", () => {
+        expect(subtract(2, 1)).toBe(1);
+    });
+    test("should divide", () => {
+        expect(divide(558, 6)).toBe(93);
+    });
+    test("should multiply", () => {
+        expect(multiply(12, 11)).toBe(132);
+    });
+    test("should show square root", () => {
+        expect(squareRoot(625)).toBe(25);
+    });
+    test("should show max", () => {
+        expect(max(2 && 1)).toBe(1);
+    });
+});
