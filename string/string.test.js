23cf5f82fda6839e00429b40528bfe08dfced79f
Heather Mosley <hmosley@student.fullsail.edu>
2022-04-10 19:19:04 -0500

    New test for the string module - all passed

---
+++ string/string.test.js
@@ -0,0 +1,12 @@
+const {uppercase, lowercase} = require("./string")
+
+describe("Testing the string module", () => {
+
+    test("should uppercase a string input", () => {
+        expect(uppercase("bob")).toBe("BOB");
+    });
+
+    test("should lowercase a string input", () => {
+        expect(lowercase("FULL SaiL")).toBe("full sail");
+    });
+});
